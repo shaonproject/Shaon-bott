a24f70ba0e794e637d3550422876477577dff21e
shaonproject <153928138+shaonproject@users.noreply.github.com>
2024-09-30 12:07:21 +0600

    Update pintarest.js

--- scripts/commands/pintarest.js
+++ scripts/commands/pintarest.js
@@ -1,53 +1,66 @@
-module.exports = {
-config: {
-    name: "pinterest",
+module.exports.config = {
+    name: "pic",
     version: "1.0.0",
     permission: 0,
-    credits: "Nayan",
+    credits: "Islamick Cyber Chat",
     description: "image search",
-    prefix: true,
-    category: "with prefix",
+    prefix: 'awto',
+    category: "image search",
     usages: "pinterest (text) - (amount)",
-    cooldowns: 10,
-},
+    cooldowns: 5,
+};
 
+module.exports.run = async function({ api, event, args }) {
 
-  languages: {
-  "vi": {
-    "missing": "phim hoạt hình - 10"
-  },
-      "en": {
-          "missing": '/pinterest anime - 10'
-      }
-  },
-
-  
-start: async function({ nayan, events, args }) {
     const axios = require("axios");
+
     const fs = require("fs-extra");
+
     const request = require("request");
+
     const keySearch = args.join(" ");
-  const { spotify, pintarest} = require('nayan-server')
-    if(keySearch.includes("-") == false) return nayan.reply(lang(" missing"), events.threadID, events.messageID)
+    
+    const apis = await axios.get('https://raw.githubusercontent.com/shaonproject/Shaon/main/api.json')
+  const Shaon = apis.data.api
+  
+    if(keySearch.includes("-") == false) return api.sendMessage('Please enter in the format, example: pinterest cat - 10 (it depends on you how many images you want to appear in the result)', event.threadID, event.messageID)
+
     const keySearchs = keySearch.substr(0, keySearch.indexOf('-'))
+
     const numberSearch = keySearch.split("-").pop() || 6
-    const res = await pintarest(`${encodeURIComponent(keySearchs)}`);
-  console.log(res)
-    const data = res.data;
+
+    const res = await axios.get(`${Shaon}/pinterest?search=${encodeURIComponent(keySearchs)}`);
+
+    const data = res.data.data;
+
     var num = 0;
+
     var imgData = [];
+
     for (var i = 0; i < parseInt(numberSearch); i++) {
+
       let path = __dirname + `/cache/${num+=1}.jpg`;
+
       let getDown = (await axios.get(`${data[i]}`, { responseType: 'arraybuffer' })).data;
+
       fs.writeFileSync(path, Buffer.from(getDown, 'utf-8'));
+
       imgData.push(fs.createReadStream(__dirname + `/cache/${num}.jpg`));
+
     }
-    nayan.reply({
+
+    api.sendMessage({
+
         attachment: imgData,
-        body: numberSearch + ' images for '+ keySearchs
-    }, events.threadID, events.messageID)
+
+        body: numberSearch + 'Search results for keyword: '+ keySearchs
+
+    }, event.threadID, event.messageID)
+
     for (let ii = 1; ii < parseInt(numberSearch); ii++) {
+
         fs.unlinkSync(__dirname + `/cache/${ii}.jpg`)
+
     }
-}
-}
+
+};
